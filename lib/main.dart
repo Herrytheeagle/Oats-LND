13b10fc12abe14039a79b223dfe11bad19a53624
yourstockinvestment <herrytheeagle@gmail.com>
2022-08-20 02:28:01 +0100

    ,,,

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
-Widget appBar( {Widget left, String title, Widget right}) {
-  
+Widget appBar({Widget left, String title, Widget right}) {
+  return Container(
+    child: Padding (
+      padding: const EdgeInsets.symmetric(horizontal : 15 , vertical : 20)
+      child: Row(
+        children
+    )
+  )
 },
