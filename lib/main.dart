99e66571b4bde29d56ff36d51313fbe5af47fd90
yourstockinvestment <herrytheeagle@gmail.com>
2022-08-22 16:23:16 +0100

    Wallet1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+
+void main() {
+  runApp(MyApp());
+},
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Digioats Wallet"
+      home: MainApp(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
